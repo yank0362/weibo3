c9fb556b522f28c37dfbf4c3a3c6f0ff2ab79cf3
Damon <yank0362@qq.com>
2019-09-23 15:05:55 +0800

    完成用户注册

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,7 @@
     @include('layouts._header')
 
     <div class="container">
+    	@include('shared._messages')
       <div class="offset-md-1 col-md-10">
         @yield('content')
         @include('layouts._footer')
