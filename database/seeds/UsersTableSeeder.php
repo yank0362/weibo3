bb8380f0e249f3ec1e0d4f5f40f535cc1a327c3d
Damon <yank0362@qq.com>
2019-09-23 17:34:26 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Damon';
         $user->email = 'yank0362@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
