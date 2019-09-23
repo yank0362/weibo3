633fb6c09e8fa88646590a732a67c02d6fe2786b
Damon <yank0362@qq.com>
2019-09-23 14:39:33 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,4 +15,15 @@ class UsersController extends Controller
     {
     	return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request,[
+    		'name'=>'required|max:50',
+    		'email'=>'required|email|max:255',
+    		'password'=>'required|confirmed|min:6'
+    	]);
+
+    	return;
+    }
 }
