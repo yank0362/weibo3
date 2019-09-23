78cd521c6e3825cec67b64e2fec17c3757811cbe
Damon <yank0362@qq.com>
2019-09-23 15:35:43 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('signup','UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
+
+
+//登录登出
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
